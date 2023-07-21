1f06a1467e60f69d728cbe447e1262c8def4df9c
Stanislau_Sukora <ssukora@estee.com>
2023-07-21 10:43:51 +0300

    Misc updates

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundedNumber = Math.round(numberToBeRounded);
+        System.out.println("Rounded number: " + roundedNumber);
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        float number = 12.75f;
+        floatTypeCasting.roundNumber(number);
     }
 }
